04d51ce9acdef7b3fd74959833c73f9eb6904c95
Matthew Dunn <mshieldsdunn@mac.com>
2015-11-12 23:28:48 +0000

    Move spec from controller to model and prefer factory creation for Game

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -1,18 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe GamesController, type: :controller do
-  describe 'games#populate_board!' do
-    it 'initializes a Black King in the correct starting position' do
-      player_black = create(:user)
-      player_white = create(:user)
-      game         = Game.create(name: 'test game', white_player: player_white, black_player: player_black)
-      black_king   = game.chess_pieces.where(position_x: 4, position_y: 0).first
-
-      expect(black_king.type).to eq 'King'
-      expect(black_king.color).to eq 'black'
-    end
-  end
-
   describe 'GET games#new' do
     it 'responds successfully with an HTTP 200 status code' do
       get :new
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -2,12 +2,20 @@ require 'rails_helper'
 
 RSpec.describe Game do
   it { is_expected.to have_attribute :name }
+  it { is_expected.to validate_presence_of :name }
 
   it { is_expected.to belong_to :white_player }
-
   it { is_expected.to belong_to :black_player }
 
   it { is_expected.to have_many :chess_pieces }
 
-  it { is_expected.to validate_presence_of :name }
+  describe 'games#populate_board!' do
+    it 'initializes a Black King in the correct starting position' do
+      game         = create(:game)
+      black_king   = game.chess_pieces.where(position_x: 4, position_y: 0).first
+
+      expect(black_king.type).to eq 'King'
+      expect(black_king.color).to eq 'black'
+    end
+  end
 end
