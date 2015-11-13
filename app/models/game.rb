d15f9febe6e49cae2a6f5e9202bd0bba547c6be0
Saroar <saroar9@gmail.com>
2015-11-13 19:43:32 +0000

    Populate a new game
    Add method to populate game in after_create callback.

--- app/models/game.rb
+++ app/models/game.rb
@@ -8,9 +8,53 @@ class Game < ActiveRecord::Base
 
   after_create :populate_board!
 
-  private
-
   def populate_board!
-    chess_pieces.create(type: 'King', position_x: 4, position_y: 0, color: :black)
+    create_knights
+    create_queens
+    create_bishops
+    create_rooks
+    create_pawns
+  end
+
+  def create_knights
+    # Create white & Black Knight
+    chess_pieces.create(type: 'Knight', position_x: 2, position_y: 1, color: :white)
+    chess_pieces.create(type: 'Knight', position_x: 7, position_y: 1, color: :white)
+    chess_pieces.create(type: 'Knight', position_x: 2, position_y: 8, color: :black)
+    chess_pieces.create(type: 'Knight', position_x: 7, position_y: 8, color: :black)
+  end
+
+  def create_queens
+    # Create White & Black Queen
+    chess_pieces.create(type: 'King', position_x: 4, position_y: 1, color: :white)
+    chess_pieces.create(type: 'King', position_x: 4, position_y: 8, color: :black)
+
+    # Create white & Black Queen
+    chess_pieces.create(type: 'Queen', position_x: 5, position_y: 1, color: :white)
+    chess_pieces.create(type: 'Queen', position_x: 5, position_y: 8, color: :black)
+  end
+
+  def create_bishops
+    # Create White & Black Bishop
+    chess_pieces.create(type: 'Bishop', position_x: 3, position_y: 1, color: :white)
+    chess_pieces.create(type: 'Bishop', position_x: 6, position_y: 1, color: :white)
+    chess_pieces.create(type: 'Bishop', position_x: 3, position_y: 8, color: :black)
+    chess_pieces.create(type: 'Bishop', position_x: 6, position_y: 8, color: :black)
+  end
+
+  def create_rooks
+    # Create white & Black Rook
+    chess_pieces.create(type: 'Rook', position_x: 1, position_y: 8, color: :white)
+    chess_pieces.create(type: 'Rook', position_x: 8, position_y: 8, color: :white)
+    chess_pieces.create(type: 'Rook', position_x: 1, position_y: 1, color: :black)
+    chess_pieces.create(type: 'Rook', position_x: 8, position_y: 1, color: :black)
+  end
+
+  def create_pawns
+    # Create White & Black Pawn
+    1.upto(8) do |x|
+      chess_pieces.create(type: 'Pawn', position_x: x, position_y: 2, color: :white)
+      chess_pieces.create(type: 'Pawn', position_x: x, position_y: 7, color: :black)
+    end
   end
 end
