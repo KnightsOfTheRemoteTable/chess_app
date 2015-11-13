f01d9e0cfba0d218089e30d6e3531e4330d43e72
Saroar <saroar9@gmail.com>
2015-11-13 19:43:32 +0000

    Populate a new game

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -10,4 +10,21 @@ RSpec.describe Game do
   it { is_expected.to have_many :chess_pieces }
 
   it { is_expected.to validate_presence_of :name }
+
+  describe 'games#populate_board!' do
+    it 'initializes a Black King in correct starting position' do
+      game = create(:game)
+      black_king = game.chess_pieces.where(position_x: 4, position_y: 0).first
+
+      expect(black_king.type).to eq 'King'
+      expect(black_king.color).to eq 'black'
+    end
+
+    it 'create White pieces location' do
+      game = create(:game)
+      correct_positions = [[1,1],[2,1],[3,1],[4,1],[5,1],[6,1],[7,1],[8,1]]
+      actual_positions = game.chess_pieces.where(type: 'Pawn').pluck(:position_x, :position_y)
+      
+    end
+  end
 end
