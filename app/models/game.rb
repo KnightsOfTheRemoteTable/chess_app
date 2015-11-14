0443aed247eb8e8861e3df749dfede2a462a1b62
Saroar <saroar9@gmail.com>
2015-11-14 21:56:28 +0000

    create pawns method for refactor code

--- app/models/game.rb
+++ app/models/game.rb
@@ -13,18 +13,10 @@ class Game < ActiveRecord::Base
     create_queens
     create_bishop
     create_rook
-
-    # Create White & Black Pawn
-    1.upto(8) do |x|
-      chess_pieces.create(type: 'Pawn', position_x: x, position_y: 2, color: :white)
-    end
-
-    1.upto(8) do |x|
-      chess_pieces.create(type: 'Pawn', position_x: x, position_y: 7, color: :black)
-    end
+    create_pawn
   end
 
-  def create_knight
+  def create_knights
     # Create white & Black Knight
     chess_pieces.create(type: 'Knight', position_x: 2, position_y: 1, color: :white)
     chess_pieces.create(type: 'Knight', position_x: 7, position_y: 1, color: :white)
@@ -42,7 +34,7 @@ class Game < ActiveRecord::Base
     chess_pieces.create(type: 'Queen', position_x: 5, position_y: 8, color: :black)
   end
 
-  def create_bishop
+  def create_bishops
     # Create White & Black Bishop
     chess_pieces.create(type: 'Bishop', position_x: 3, position_y: 1, color: :white)
     chess_pieces.create(type: 'Bishop', position_x: 6, position_y: 1, color: :white)
@@ -50,11 +42,22 @@ class Game < ActiveRecord::Base
     chess_pieces.create(type: 'Bishop', position_x: 6, position_y: 8, color: :black)
   end
 
-  def create_rook
+  def create_rooks
     # Create white & Black Rook
     chess_pieces.create(type:  'Rook', position_x: 1, position_y: 8, color: :white)
     chess_pieces.create(type:  'Rook', position_x: 8, position_y: 8, color: :white)
     chess_pieces.create(type:  'Rook', position_x: 1, position_y: 1, color: :black)
     chess_pieces.create(type:  'Rook', position_x: 8, position_y: 1, color: :black)
+  end
+
+  def create_pawns
+    # Create White & Black Pawn
+    1.upto(8) do |x|
+      chess_pieces.create(type: 'Pawn', position_x: x, position_y: 2, color: :white)
+    end
+
+    1.upto(8) do |x|
+      chess_pieces.create(type: 'Pawn', position_x: x, position_y: 7, color: :black)
+    end
   end
 end
