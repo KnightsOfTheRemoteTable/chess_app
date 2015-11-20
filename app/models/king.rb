1ab50db7784c8ed2a7d70a314e59f070733e0df2
Matthew Dunn <mshieldsdunn@mac.com>
2015-11-20 09:34:04 +0000

    Create passing spec for King#valid_move?

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,2 +1,19 @@
 class King < ChessPiece
+  X_MOVE_FACTOR = 1
+  Y_MOVE_FACTOR = 1
+
+  def valid_move?(destination_x, destination_y)
+    fail ArgumentError, 'Invalid Move' if obstructed?(destination_x, destination_y)
+    exceeds_x_movement?(destination_x) || exceeds_y_movement?(destination_y) ? false : true
+  end
+
+  private
+
+  def exceeds_x_movement?(destination_x)
+    (destination_x - position_x).abs > X_MOVE_FACTOR
+  end
+
+  def exceeds_y_movement?(destination_y)
+    (destination_y - position_y).abs > Y_MOVE_FACTOR
+  end
 end
--- spec/models/king_spec.rb
+++ spec/models/king_spec.rb
@@ -4,4 +4,24 @@ RSpec.describe King do
   it 'has type of King' do
     expect(subject.type).to eq 'King'
   end
+
+  describe '#valid_move?' do
+    it 'is a public instance method' do
+      expect(subject).to respond_to :valid_move?
+    end
+
+    context 'when a proposed move is within the movement abilities of the piece' do
+      it 'returns true' do
+        king = create(:king, position_x: 4, position_y: 4)
+        expect(king.valid_move?(5, 5)).to be true
+      end
+    end
+
+    context 'when a proposed move is not within the movement abilities of the piece' do
+      it 'returns false' do
+        king = create(:king, position_x: 4, position_y: 4)
+        expect(king.valid_move?(6, 6)).to be false
+      end
+    end
+  end
 end
