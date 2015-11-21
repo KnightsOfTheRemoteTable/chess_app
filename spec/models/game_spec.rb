7726fe9186a8d1faa95259d2d32652d7fa0bf69d
Seth Allen <seth.a@me.com>
2015-11-21 19:27:43 +0000

    Fix King and Queen x and y position, fix Rook color

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -15,15 +15,15 @@ RSpec.describe Game do
     it 'initializes a Black & White King in correct starting position' do
       game = create(:game)
 
-      expect(game.chess_pieces.find_by(type: 'King', position_x: 4, position_y: 1).color).to eq 'white'
-      expect(game.chess_pieces.find_by(type: 'King', position_x: 4, position_y: 8).color).to eq 'black'
+      expect(game.chess_pieces.find_by(type: 'King', position_x: 5, position_y: 1).color).to eq 'white'
+      expect(game.chess_pieces.find_by(type: 'King', position_x: 5, position_y: 8).color).to eq 'black'
     end
 
     it 'initializes a Black & White Queen in correct starting position' do
       game = create(:game)
 
-      expect(game.chess_pieces.find_by(type: 'Queen', position_x: 5, position_y: 1).color).to eq 'white'
-      expect(game.chess_pieces.find_by(type: 'Queen', position_x: 5, position_y: 8).color).to eq 'black'
+      expect(game.chess_pieces.find_by(type: 'Queen', position_x: 4, position_y: 1).color).to eq 'white'
+      expect(game.chess_pieces.find_by(type: 'Queen', position_x: 4, position_y: 8).color).to eq 'black'
     end
 
     it 'initializes a Black & White Bishop in correct starting position' do
@@ -38,10 +38,10 @@ RSpec.describe Game do
     it 'initializes a Black & White Rook in correct starting position' do
       game = create(:game)
 
-      expect(game.chess_pieces.find_by(type: 'Rook', position_x: 1, position_y: 8).color).to eq 'white'
-      expect(game.chess_pieces.find_by(type: 'Rook', position_x: 8, position_y: 8).color).to eq 'white'
-      expect(game.chess_pieces.find_by(type: 'Rook', position_x: 1, position_y: 1).color).to eq 'black'
-      expect(game.chess_pieces.find_by(type: 'Rook', position_x: 8, position_y: 1).color).to eq 'black'
+      expect(game.chess_pieces.find_by(type: 'Rook', position_x: 1, position_y: 8).color).to eq 'black'
+      expect(game.chess_pieces.find_by(type: 'Rook', position_x: 8, position_y: 8).color).to eq 'black'
+      expect(game.chess_pieces.find_by(type: 'Rook', position_x: 1, position_y: 1).color).to eq 'white'
+      expect(game.chess_pieces.find_by(type: 'Rook', position_x: 8, position_y: 1).color).to eq 'white'
     end
 
     it 'initializes a Black & White Pawn in correct starting position' do
