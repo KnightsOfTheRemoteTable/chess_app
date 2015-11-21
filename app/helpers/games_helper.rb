d6ff52d5f492bad2b89c87107e3544ac75657a65
Seth Allen <seth.a@me.com>
2015-11-21 20:13:20 +0000

    Move draw_piece to new method

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,15 +1,18 @@
 module GamesHelper
   def draw_square(x_axis, y_axis)
-    current_piece = @chess_pieces.find_by(position_x: x_axis, position_y: y_axis)
     if x_axis.odd? && y_axis.odd? || x_axis.even? && y_axis.even?
       color = 'lite'
     else
       color = 'dark'
     end
     content_tag :div, class: "chessboard__row__space chessboard__row__space--#{color}" do
-      if current_piece.present?
-        image_tag "#{current_piece.color.downcase}_#{current_piece.type.downcase}.png"
-      end
+      draw_piece(x_axis, y_axis)
     end
+  end
+
+  def draw_piece(x_axis, y_axis)
+    current_piece = @chess_pieces.find_by(position_x: x_axis, position_y: y_axis)
+    return unless current_piece.present?
+    image_tag "#{current_piece.color.downcase}_#{current_piece.type.downcase}.png"
   end
 end
