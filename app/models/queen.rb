fa3d0bf0a1d1ca3c98bb0ac7752921ff3284fe88
Jonathan Mast <jon@padorpers.com>
2015-11-21 12:38:36 -0500

    Add valid_move? method to Queen
    This returns true if the move is in a straight line and not obstructed.

--- app/models/queen.rb
+++ app/models/queen.rb
@@ -1,2 +1,8 @@
 class Queen < ChessPiece
+  def valid_move?(destination_x, destination_y)
+    return false unless vertical_move?(destination_x, destination_y) ||
+                        horizontal_move?(destination_x, destination_y) ||
+                        diagonal_move?(destination_x, destination_y)
+    !obstructed?(destination_x, destination_y)
+  end
 end
--- spec/models/queen_spec.rb
+++ spec/models/queen_spec.rb
@@ -4,4 +4,30 @@ RSpec.describe Queen do
   it 'has type of Queen' do
     expect(subject.type).to eq 'Queen'
   end
+
+  describe '#valid_move?' do
+    let(:queen) { FactoryGirl.create(:queen, position_x: 3, position_y: 3) }
+
+    it 'returns true for vertical moves' do
+      expect(queen.valid_move?(3, 6)).to eq true
+    end
+
+    it 'returns true for horizontal moves' do
+      expect(queen.valid_move?(4, 3)).to eq true
+    end
+
+    it 'returns true for diagonal moves' do
+      expect(queen.valid_move?(1, 5)).to eq true
+    end
+
+    it 'returns false for moves that are not straight lines' do
+      expect(queen.valid_move?(4, 5)).to eq false
+    end
+
+    it 'returns false if obstructed' do
+      FactoryGirl.create(:pawn, position_x: 3, position_y: 4, game: queen.game)
+
+      expect(queen.valid_move?(3, 5)).to eq false
+    end
+  end
 end
