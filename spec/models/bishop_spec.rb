855a6ac55185d40b4e3ec165a5eae2292495b469
Matthew Dunn <mshieldsdunn@mac.com>
2015-11-22 01:16:54 +0000

    Update spec to make obstruction explicit

--- spec/models/bishop_spec.rb
+++ spec/models/bishop_spec.rb
@@ -6,19 +6,19 @@ RSpec.describe Bishop do
   end
 
   describe '#valid_move?' do
-    let(:bishop) { create(:bishop, position_x: 3, position_y: 3) }
+    let(:bishop) { create(:bishop, position_x: 5, position_y: 5) }
 
-    it 'returns true for vertical moves' do
+    it 'returns true for diagonal moves' do
       expect(bishop.valid_move?(6, 6)).to eq true
     end
 
-    it 'returns false for non-diagonal lines' do
+    it 'returns false for non-diagonal moves' do
       expect(bishop.valid_move?(6, 7)).to eq false
     end
 
     it 'returns false if obstructed' do
-      create(:pawn, position_x: 7, position_y: 7)
-      expect(bishop.valid_move?(8, 8)).to eq false
+      create(:pawn, position_x: 4, position_y: 4, game: bishop.game)
+      expect(bishop.valid_move?(3, 3)).to eq false
     end
   end
 end
