eb61baad85a5bf4bdad36fc2b163a105460742d1
Matthew Dunn <mshieldsdunn@mac.com>
2015-11-22 06:34:44 +0000

    Add passing specs for #valid_move? for Knight

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,2 +1,17 @@
 class Knight < ChessPiece
+  XY_MOVE_FACTOR = 1
+  ALT_XY_MOVE_FACTOR = 2
+
+  def valid_move?(destination_x, destination_y)
+    return false if out_of_bounds?(destination_x, destination_y)
+    return false unless x_and_y_are_offset?(destination_x, destination_y)
+    true
+  end
+
+  private
+
+  def x_and_y_are_offset?(destination_x, destination_y)
+    ((destination_x - position_x == XY_MOVE_FACTOR) && (destination_y - position_y == ALT_XY_MOVE_FACTOR)) ||
+      ((destination_y - position_y == XY_MOVE_FACTOR) && (destination_x - position_x == ALT_XY_MOVE_FACTOR))
+  end
 end
--- spec/models/knight_spec.rb
+++ spec/models/knight_spec.rb
@@ -4,4 +4,25 @@ RSpec.describe Knight do
   it 'has type of Knight' do
     expect(subject.type).to eq 'Knight'
   end
+
+  describe '#valid_move?' do
+    let(:knight) { create(:knight, position_x: 4, position_y: 4) }
+
+    it 'returns true if the x and y move components are 1 and 2, respectively' do
+      expect(knight.valid_move?(5, 6)).to eq true
+    end
+
+    it 'returns true if the x and y move components are 2 and 1, respectively' do
+      expect(knight.valid_move?(6, 5)).to eq true
+    end
+
+    it 'returns false if out of bounds' do
+      knight = create(:knight, position_x: 7, position_y: 4)
+      expect(knight.valid_move?(9, 5)).to eq false
+    end
+
+    it 'returns false otherwise' do
+      expect(knight.valid_move?(6, 6)).to eq false
+    end
+  end
 end
