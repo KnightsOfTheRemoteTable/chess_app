cd317c06105228942d08ee3f5093b77a3d5c669b
Daniel Rotenberg <Daniel.Rotenberg88@gmail.com>
2015-11-25 16:35:33 -0500

    List of games on hompage are now clickable

--- spec/features/user_visits_homepage_spec.rb
+++ spec/features/user_visits_homepage_spec.rb
@@ -11,4 +11,17 @@ feature 'user visits the homepage of the application' do
     visit root_path
     expect(page).to have_css('.game')
   end
+
+  scenario 'new games appear on homepage' do
+    create(:game, name: "fancy test game")
+    visit root_path
+    expect(page).to have_link("fancy test game")
+  end
+
+  scenario 'after clicking game, user is on game page' do
+    game = create(:game, name: "click me test game")
+    visit root_path
+    click_link("click me test game")
+    expect(current_path).to eq game_path(game.id)
+  end
 end
