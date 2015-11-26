123a7ff650e68b9f89973430436ed4d891547eb3
Matthew Dunn <mshieldsdunn@mac.com>
2015-11-26 15:48:20 -0800

    Create passing spec for Rook

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -1,7 +1,8 @@
 class Rook < ChessPiece
   def valid_move?(destination_x, destination_y)
     return false if out_of_bounds?(destination_x, destination_y)
-    return false if obstruction?(destination_x, destination_y)
-    horizontal_move?(destination_x, destination_y) || vertical_move(destination_x, destination_y)
+    return false if diagonal_move?(destination_x, destination_y)
+    return false if obstructed?(destination_x, destination_y)
+    horizontal_move?(destination_x, destination_y) || vertical_move?(destination_x, destination_y) ? true : false
   end
 end
