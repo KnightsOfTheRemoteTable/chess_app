e2898aaa07fba1e5de25e21e6b481250259fd2e4
Matthew Dunn <mshieldsdunn@mac.com>
2015-11-26 15:12:55 -0800

    Add boundaries checks

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -1,5 +1,6 @@
 class Rook < ChessPiece
   def valid_move?(destination_x, destination_y)
+    return false if out_of_bounds?(destination_x, destination_y)
     return false if obstruction?(destination_x, destination_y)
     horizontal_move?(destination_x, destination_y) || vertical_move(destination_x, destination_y)
   end
