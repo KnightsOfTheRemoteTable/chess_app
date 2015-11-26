2aa04ffd301c873fd22fb96cd3b3a962956810e7
Saroar <saroar9@gmail.com>
2015-11-26 21:25:38 +0000

    horizontal or vertical move, non move, and no obstruction

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -1,2 +1,14 @@
 class Rook < ChessPiece
+  def valid_move?(destination_x, destination_y)
+    # Non move
+    return false if destination_x == self.position_x && destination_y == self.position_y
+
+    # no obstruction
+    return false if obstruction?(destination_x, destination_y) == true
+
+    # horizontal or vertical move
+    return false if (destination_x < self.position_x || destination_x > self.position_y) && ( destination_y < self.position_y || destination_y > self.position_y)
+
+    return true
+  end
 end
--- spec/models/rook_spec.rb
+++ spec/models/rook_spec.rb
@@ -4,4 +4,16 @@ RSpec.describe Rook do
   it 'has type of Rook' do
     expect(subject.type).to eq 'Rook'
   end
+
+  describe '#valid_move?' do
+    let(:rook) { create(:rook, position_x: 1, position_y: 8)}
+
+    it "return true when valid move" do
+
+    end
+
+    it "return false when invalid move" do
+
+    end
+  end
 end
