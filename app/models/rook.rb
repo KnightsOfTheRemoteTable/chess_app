dba32479382a84b0dc58cf85973cffd42f16d923
Matthew Dunn <mshieldsdunn@mac.com>
2015-11-26 15:11:02 -0800

    Remove unnecessary check for non-move

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -1,11 +1,6 @@
 class Rook < ChessPiece
   def valid_move?(destination_x, destination_y)
-    return false if destination_matches_origin(destination_x, destination_y)
     return false if obstruction?(destination_x, destination_y)
     horizontal_move?(destination_x, destination_y) || vertical_move(destination_x, destination_y)
-  end
-
-  def destination_matches_origin(destination_x, destination_y)
-    destination_x == position_x && destination_y == position_y
   end
 end
