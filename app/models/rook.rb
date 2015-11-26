eb571240c99d649db554fadd84112c3dbfa5ce73
Saroar <saroar9@gmail.com>
2015-11-26 21:46:18 +0000

    3 offenses detected

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -1,14 +1,15 @@
 class Rook < ChessPiece
   def valid_move?(destination_x, destination_y)
     # Non move
-    return false if destination_x == self.position_x && destination_y == self.position_y
+    return false if destination_x == position_x && destination_y == position_y
 
     # no obstruction
     return false if obstruction?(destination_x, destination_y) == true
 
     # horizontal or vertical move
-    return false if (destination_x < self.position_x || destination_x > self.position_y) && ( destination_y < self.position_y || destination_y > self.position_y)
+    return false if (destination_x < position_x || destination_x > position_y) &&
+                    (destination_y < position_y || destination_y > position_y)
 
-    return true
+    true
   end
 end
--- spec/models/rook_spec.rb
+++ spec/models/rook_spec.rb
@@ -6,14 +6,5 @@ RSpec.describe Rook do
   end
 
   describe '#valid_move?' do
-    let(:rook) { create(:rook, position_x: 1, position_y: 8)}
-
-    it "return true when valid move" do
-
-    end
-
-    it "return false when invalid move" do
-
-    end
   end
 end
