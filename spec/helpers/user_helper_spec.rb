4b50c9f29c80ef6fe1c9ad9fd300c5645b04d74d
Saroar <saroar9@gmail.com>
2015-12-03 20:40:46 +0000

    passed gravatar test

---
+++ spec/helpers/user_helper_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe UsersHelper, :type => :helper do
+  describe '#gravatar_for' do
+    it 'gravatar image_tag' do
+      user = create(:user)
+      gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+      gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{40}"
+
+      expect(helper.gravatar_for(user)).to eq image_tag(gravatar_url, alt: user.username, class: 'gravatar')
+    end
+  end
+end
