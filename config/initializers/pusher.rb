eb5814a77738308c9ed46c602726d6cc25d8c2a0
Matthew Dunn <mshieldsdunn@mac.com>
2015-12-12 14:38:29 -0800

    Change name of environment variable

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,6 +1,6 @@
 require 'pusher'
 
-Pusher.url    = ENV['pusher_url']
+Pusher.url    = ENV['pusher_URL']
 Pusher.app_id = ENV['pusher_app_id']
 Pusher.key    = ENV['pusher_key']
 Pusher.secret = ENV['pusher_secret']
