84d20320a5935c740fa1d7de383c7584fd07e5ff
Matthew Dunn <mshieldsdunn@mac.com>
2015-12-14 12:04:47 -0800

    Fix spec

--- spec/features/user_visits_homepage_spec.rb
+++ spec/features/user_visits_homepage_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 feature 'user visits the homepage of the application' do
   scenario 'successfully' do
     visit root_path
-    expect(page).to have_css 'h1', text: 'best chess game ever'
+    expect(page).to have_css 'h1', text: 'Online Chess'
   end
 end
