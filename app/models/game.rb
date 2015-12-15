183a464e8c787a8a95ab892dca7e065ed1377722
Jonathan Mast <jon@padorpers.com>
2015-12-15 15:52:48 -0500

    Refactor board populating logic to reduce duplication
    Create an array with the names of the home rank pieces and loop over
    them to populate the board.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,15 @@
 class Game < ActiveRecord::Base
+  HOME_RANK = %w(
+    Rook
+    Knight
+    Bishop
+    King
+    Queen
+    Bishop
+    Knight
+    Rook
+  )
+
   belongs_to :white_player, class_name: 'User'
   belongs_to :black_player, class_name: 'User'
   belongs_to :winner, class_name: 'User'
@@ -28,50 +39,9 @@ class Game < ActiveRecord::Base
   end
 
   def populate_board!
-    create_knights
-    create_queens
-    create_bishops
-    create_rooks
+    create_home_rank
     create_pawns
     current_player_is_white_player!
-  end
-
-  def create_knights
-    chess_pieces.create(type: 'Knight', position_x: 2, position_y: 1, color: :white)
-    chess_pieces.create(type: 'Knight', position_x: 7, position_y: 1, color: :white)
-    chess_pieces.create(type: 'Knight', position_x: 2, position_y: 8, color: :black)
-    chess_pieces.create(type: 'Knight', position_x: 7, position_y: 8, color: :black)
-  end
-
-  def create_queens
-    # Create White & Black Queen
-    chess_pieces.create(type: 'King', position_x: 4, position_y: 1, color: :white)
-    chess_pieces.create(type: 'King', position_x: 4, position_y: 8, color: :black)
-
-    # Create white & Black Queen
-    chess_pieces.create(type: 'Queen', position_x: 5, position_y: 1, color: :white)
-    chess_pieces.create(type: 'Queen', position_x: 5, position_y: 8, color: :black)
-  end
-
-  def create_bishops
-    chess_pieces.create(type: 'Bishop', position_x: 3, position_y: 1, color: :white)
-    chess_pieces.create(type: 'Bishop', position_x: 6, position_y: 1, color: :white)
-    chess_pieces.create(type: 'Bishop', position_x: 3, position_y: 8, color: :black)
-    chess_pieces.create(type: 'Bishop', position_x: 6, position_y: 8, color: :black)
-  end
-
-  def create_rooks
-    chess_pieces.create(type: 'Rook', position_x: 1, position_y: 8, color: :black)
-    chess_pieces.create(type: 'Rook', position_x: 8, position_y: 8, color: :black)
-    chess_pieces.create(type: 'Rook', position_x: 1, position_y: 1, color: :white)
-    chess_pieces.create(type: 'Rook', position_x: 8, position_y: 1, color: :white)
-  end
-
-  def create_pawns
-    1.upto(8) do |x|
-      chess_pieces.create(type: 'Pawn', position_x: x, position_y: 2, color: :white)
-      chess_pieces.create(type: 'Pawn', position_x: x, position_y: 7, color: :black)
-    end
   end
 
   def state_of_stalemate?(color)
@@ -127,5 +97,19 @@ class Game < ActiveRecord::Base
       return true if opponent.valid_move?(king.coordinates)
     end
     false
+  end
+
+  def create_home_rank
+    HOME_RANK.each_with_index do |type, index|
+      chess_pieces.create(type: type, position_x: index + 1, position_y: 1, color: :white)
+      chess_pieces.create(type: type, position_x: index + 1, position_y: 8, color: :black)
+    end
+  end
+
+  def create_pawns
+    1.upto(8) do |x|
+      chess_pieces.create(type: 'Pawn', position_x: x, position_y: 2, color: :white)
+      chess_pieces.create(type: 'Pawn', position_x: x, position_y: 7, color: :black)
+    end
   end
 end
